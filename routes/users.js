6f58794ad40af4fca6d3a678765dd1db58b7bf62
Terence Rajapaksage <terenceraja@hotmail.fr>
2023-04-27 22:24:31 +0200

    testing routes fini

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,7 @@ router.post('/signin', (req, res) => {
   User.findOne({ username: req.body.username }).then(data => {
     console.log(data)
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token });
+      res.json({ result: true, user: data });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
