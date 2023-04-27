0e2e61a683a2e348cb5de8f3496663bc594101cc
Benoit <giannini.benoit@gmail.comwd>
2023-04-27 17:33:44 +0200

    finalisation route page login, ajout du checkbody pour verification v2

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,14 @@ const User = require('../models/users');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const uid2 = require('uid2');
+const { checkBody } = require('../modules/checkBody');
+
 
 /* POST route pour inscripton */
 router.post('/signup', function(req, res) {
+  if (!checkBody(req.body, ['username', 'password', 'fisrtname'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+  } else {
   User.findOne({username: req.body.username})
   .then (data => {
     if (data) {
@@ -24,24 +29,24 @@ router.post('/signup', function(req, res) {
     res.json({result: true, user: newUser});
   })
 }}
-)}
+)}}
 );
 
 /* POST route pour co */
 router.post('/signin', (req, res) => {
   if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
-    return;
-  }
-
+  } else {
   User.findOne({ username: req.body.username }).then(data => {
+    console.log(data)
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
-  });
-});
+  })
+}}
+);
 
 
 module.exports = router;
