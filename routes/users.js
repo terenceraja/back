c13985ef31c8edcfbcfb4c3e2343a82756a90900
Benoit <giannini.benoit@gmail.comwd>
2023-04-27 16:57:19 +0200

    ajout condition if pour post si user already exist, sinon création du compte

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,44 @@ var router = express.Router();
 const bcrypt = require('bcrypt');
 const uid2 = require('uid2');
 
-/* GET users listing. */
+/* POST route pour inscripton */
 router.post('/signup', function(req, res) {
-  const hash = bcrypt.hashSync(req.body.password, 10);
-  const token = uid2(32);
-  const newUser = new User({
-    firstname: req.body.firstname, 
-    username: req.body.username,
-    canDelete: true,
-    token: token, 
-    password: hash,
+  User.findOne({username: req.body.username})
+  .then (data => {
+    if (data) {
+      res.json({result: false, message: 'User already exist'})
+    } else {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const token = uid2(32);
+      const newUser = new User({
+        firstname: req.body.firstname, 
+        username: req.body.username,
+        canDelete: true,
+        token: token, 
+        password: hash,
   });
   newUser.save().then(newUser => {
     res.json({result: true, user: newUser});
+  })
+}}
+)}
+);
+
+/* POST route pour co */
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
   });
 });
+
 
 module.exports = router;
