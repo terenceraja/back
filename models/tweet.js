a7ab571527e784375cd06feeb518720165a42fda
Terence Rajapaksage <terenceraja@hotmail.fr>
2023-04-28 11:02:45 +0200

    routes tweet

---
+++ models/tweet.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = mongoose.Schema({
+  tweet: String,
+  token: String,
+});
+
+const Tweet = mongoose.model("tweets", tweetSchema);
+
+module.exports = Tweet;
